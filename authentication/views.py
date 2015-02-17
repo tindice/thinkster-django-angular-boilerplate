bebe7ab0240a94f7bffadbbdc746cac0431da025
Martin Leibner <tincorad@gmail.com>
2015-02-17 01:53:18 -0200

    Ultimos cambios login/logout

--- authentication/views.py
+++ authentication/views.py
@@ -1,12 +1,10 @@
-from django.shortcuts import render
-
-# Create your views here.
 from rest_framework import permissions, viewsets
 
 from authentication.models import Account
 from authentication.permissions import IsAccountOwner
 from authentication.serializers import AccountSerializer
 
+# Create your views here.
 
 class AccountViewSet(viewsets.ModelViewSet):
     lookup_field = 'username'
@@ -34,3 +32,54 @@ class AccountViewSet(viewsets.ModelViewSet):
             'status': 'Bad request',
             'message': 'Account could not be created with received data.'
         }, status=status.HTTP_400_BAD_REQUEST)
+
+
+#-----------------------------------------------------------------------------#
+# Making the login API view
+
+import json
+
+from django.contrib.auth import authenticate, login
+
+from rest_framework import status, views
+from rest_framework.response import Response
+
+class LoginView(views.APIView):
+    def post(self, request, format=None):
+        data = json.loads(request.body)
+
+        email = data.get('email', None)
+        password = data.get('password', None)
+
+        account = authenticate(email=email, password=password)
+
+        if account is not None:
+            if account.is_active:
+                login(request, account)
+
+                serialized = AccountSerializer(account)
+
+                return Response(serialized.data)
+            else:
+                return Response({
+                    'status': 'Unauthorized',
+                    'message': 'This account has been disabled.'
+                }, status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            return Response({
+                'status': 'Unauthorized',
+                'message': 'Username/password combination invalid.'
+            }, status=status.HTTP_401_UNAUTHORIZED)
+
+#--------------------------------------------------------------------------------#
+from django.contrib.auth import logout
+
+from rest_framework import permissions
+
+class LogoutView(views.APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request, format=None):
+        logout(request)
+
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
--- static/javascripts/authentication/controllers/register.controller.js
+++ static/javascripts/authentication/controllers/register.controller.js
@@ -27,5 +27,17 @@
     function register() {
       Authentication.register(vm.email, vm.password, vm.username);
     }
+
+    /**
+     * @name activate
+     * @desc Actions to be performed when this controller is instantiated
+     * @memberOf thinkster.authentication.controllers.RegisterController
+     */
+    function activate() {
+      // If the user is authenticated, they should not be here.
+      if (Authentication.isAuthenticated()) {
+        $location.url('/');
+      }
+    }
   }
 })();
